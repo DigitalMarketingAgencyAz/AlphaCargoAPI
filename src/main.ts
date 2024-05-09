b015b06e9aecc36d4340a84583a3b56778d08dc6
TheErl <tynis45@gmail.com>
2024-05-10 00:17:32 +0600

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const options = new DocumentBuilder()
     .setTitle('Your API Title')
     .setDescription('Your API description')
